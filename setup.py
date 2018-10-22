5da7f5d37563ece111dcd43ee5e53eebe5c35860
Daniel Hara <haradaniel734@gmail.com>
2018-10-22 18:19:35 -0300

    setup.py file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='Forex-On-Whatsapp',
+    version='0.1',
+    description='App to send forex rates to Whatsapp numbers',
+    author='Daniel Hara',
+    author_email='haradaniel734@gmail.com',
+)
