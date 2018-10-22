c04bf2e87f5bd02831b7599226818e120c4533e0
Daniel Hara <haradaniel734@gmail.com>
2018-10-22 15:22:44 -0300

    delete useless functin

--- src/database/databaseservice.py
+++ src/database/databaseservice.py
@@ -7,20 +7,3 @@ class DatabaseService:
 
         self.client = MongoClient(CONNECTION_STRING)
         self.model = self.client['bitcoin-alert-database']
-
-    def create_dump_database(self):
-        users = self.model.users
-
-        user = {
-            'name': 'Daniel Hara',
-            'phone_number': '5511964498206',
-            'assets': [
-                'USD',
-                'EUR',
-                'BRL',
-                'BIT'
-            ]
-        }
-        user_id = users.insert_one(user).inserted_id
-        if user_id:
-            print('inserted successfully!')
